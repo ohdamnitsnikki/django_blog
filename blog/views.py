fc1f4976d5ddd6d9a3b8044b596c1e8b106f651e
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-04-17 20:42:22 +0000

    add comments

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ class PostList(generic.ListView):
 
 class PostDetail(View):
 
-        def get(self, request, slug, *args, **kwargs):
+    def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
