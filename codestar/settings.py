e55217a61bf2a2997510f82c02d70e619a7e9f3f
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-04-18 12:05:16 +0000

    add deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
-
 import dj_database_url
 from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"):
@@ -32,7 +31,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTION = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["django-blog4827.herokuapp.com", "localhost"]
 
@@ -62,7 +63,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-MESSAGE_TAGS ={
+MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
     messages.INFO: 'alert-info',
     messages.SUCCESS: 'alert-success',
@@ -70,7 +71,7 @@ MESSAGE_TAGS ={
     messages.ERROR: 'alert-danger',
 }
 
-CRISPY_TEMPLATE_PACK ='bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -159,7 +160,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]	
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
